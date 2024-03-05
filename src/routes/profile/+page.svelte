d292c5be567fd109c2576f02758734f9727d4ac3
030820acc <030820acc@gmail.com>
2024-03-04 22:39:16 -0600

    changed profile a little

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,28 +1,18 @@
 <script>
     import Navbar from "$lib/Navbar.svelte";
+    let profile = {
+        userId: 1,
+        userName: "slemonacc",
+        passwordEncrpt: "578727344",
 
+    }
 
 </script>
 
 <Navbar />
-<p>this will be the profile page</p>
+<h1>{profile.userName}</h1>
+
 
 <style>
-    p {
-        background-color: brown;
-        color: white;
-        padding: 3em;
-        border: white 1px solid;
-        border-radius: 50px;
-        height: 5em;
-        width: 10em;
-        transition: color 1s, background-color 1s, height 1s, width 1s;
-    }
-
-    p:hover {
-        background-color: black;
-        color: white;
-        height: 6em;
-        width: 11em;
-    }
+    
 </style>
